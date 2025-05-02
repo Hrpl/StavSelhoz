19367f5c6b037baa1a1ae548285f6ef6e060b323
Denis <ponomarevd61@gmail.com>
2025-05-02 19:57:47 +0300

    fix bag

--- StavSelhoz.Infrastructure/Services/Implementations/DbConnectionManager.cs
+++ StavSelhoz.Infrastructure/Services/Implementations/DbConnectionManager.cs
@@ -17,7 +17,7 @@ public class DbConnectionManager : IDbConnectionManager
     {
         _logger = logger;
     }
-    private string NpgsqlConnectionString => $"Host=localhost;Port=5432;Database=StavSelhoz;Username=postgres;Password=postgres;";
+    private string NpgsqlConnectionString => $"Host=localhost;Port=5432;Database=StavSelhoz;Username=postgres;Password=2208;";
 
 
     public NpgsqlConnection PostgresDbConnection => new(NpgsqlConnectionString);
